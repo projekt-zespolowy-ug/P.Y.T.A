c77040bc2d003ff178467770493ec8a7344fd188
Malek23 <mikolajmalek10@wp.pl>
2024-11-07 00:51:34 +0100

    fix(backend): [PYTA-20] Fix

--- backend/app/core/utils/querying_utils.py
+++ backend/app/core/utils/querying_utils.py
@@ -1,9 +1,11 @@
+from typing import Any
+
 import bcrypt
 
 
 class QueryingUtils:
-    @staticmethod
-    def hash_password(password: str) -> str:
-        password_bytes = password.encode("utf-8")
-        hashed_bytes = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
-        return hashed_bytes.decode("utf-8")
+	@staticmethod
+	def hash_password(password: Any) -> Any:
+		password_bytes = password.encode("utf-8")
+		hashed_bytes = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+		return hashed_bytes.decode("utf-8")
