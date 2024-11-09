e579c3e83239f16db53f2694a3de18192f38e7ed
malekk1 <118076432+malekk1@users.noreply.github.com>
2024-11-09 13:17:51 +0100

    feat(backend): [PYTA-20] Add dummy data (#24)
    Co-authored-by: Memetelve <memetelve1@gmail.com>

--- backend/app/core/models/auth.py
+++ backend/app/core/models/auth.py
@@ -1,14 +1,23 @@
+from typing import Any
+
 from peewee import ForeignKeyField, TextField
 
 from app.core.models.base_model import BaseModel, cuid_generator
 from app.core.models.role import Role
+from app.core.utils.querying_utils import QueryingUtils
 
 
 class Auth(BaseModel):
 	id = TextField(primary_key=True, default=cuid_generator)
 	role_id = ForeignKeyField(Role)
 	email = TextField()
-	password = TextField()
+	password: str = TextField()  # type: ignore [assignment]
 
 	class Meta:
 		table_name = "auth"
+
+	def save(self, force_insert: bool = False, only: Any | None = None) -> Any:
+		if self.password:
+			self.password = QueryingUtils.hash_password(self.password)
+
+		super().save(force_insert=force_insert, only=only)
--- backend/app/core/utils/init_db.py
+++ backend/app/core/utils/init_db.py
@@ -1,3 +1,7 @@
+import random
+
+import requests
+
 from app.core.models import (
 	Auth,
 	Company,
@@ -8,6 +12,7 @@ from app.core.models import (
 	Transaction,
 	User,
 )
+from app.core.models.role import RoleType
 from app.database import db
 
 db.connect()
@@ -16,6 +21,12 @@ db.connect()
 class InitDB:
 	def __init__(self) -> None:
 		self.create_tables()
+		self.add_role_data()
+		self.add_auth_data()
+		self.add_user_data()
+		self.add_dummy_data("https://pastebin.com/raw/KBVnSJtp", Exchange, [])
+		self.add_dummy_data("https://pastebin.com/raw/HH5uPbia", Industry, [])
+		self.add_dummy_data("https://pastebin.com/raw/fmbJktwU", Company, [Exchange, Industry])
 
 	def create_tables(self) -> None:
 		db.create_tables(
@@ -31,3 +42,51 @@ class InitDB:
 			],
 			safe=True,
 		)
+
+	def add_dummy_data(
+		self,
+		url: str,
+		model: type[Company] | type[Exchange] | type[Industry],
+		foreign_keys: list[type[Exchange] | type[Industry]],
+	) -> None:
+		if model.select().count():
+			return
+		data = requests.get(url, verify=False).json()
+		with db.atomic():
+			for record in data:
+				model.create(
+					**record,
+					**{
+						key.__name__.lower() + "_id": random.choice(list(key.select())).id
+						for key in foreign_keys
+					},
+				)
+
+	def add_role_data(self) -> None:
+		if Role.select().count():
+			return
+		with db.atomic():
+			for role in RoleType:
+				Role.create(role=role.value)
+
+	def add_auth_data(self) -> None:
+		if Auth.select().count():
+			return
+		roles = list(Role.select())
+		auth_data = requests.get("https://pastebin.com/raw/Mmf4yLr8", verify=False).json()
+		with db.atomic():
+			for record in auth_data:
+				random_role = random.choice(roles)
+				Auth.create(
+					**record,
+					role_id=random_role,
+				)
+
+	def add_user_data(self) -> None:
+		if User.select().count():
+			return
+		auth_records = list(Auth.select())
+		user_data = requests.get("https://pastebin.com/raw/upc7sbBN", verify=False).json()
+		with db.atomic():
+			for index, record in enumerate(user_data):
+				User.create(**record, auth_id=auth_records[index])
