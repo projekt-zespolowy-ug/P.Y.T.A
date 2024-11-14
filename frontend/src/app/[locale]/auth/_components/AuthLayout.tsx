e05af20b1fe08fbcbc50eae9becb4fde2727109b
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2024-11-14 18:34:25 +0100

    feat(frontend): [NO-ISSUE] test

--- frontend/src/app/[locale]/auth/_components/AuthLayout.tsx
+++ frontend/src/app/[locale]/auth/_components/AuthLayout.tsx
@@ -5,21 +5,22 @@ import SignInForm from "./SignInForm";
 import SignUpForm from "./SignUpForm";
 
 const AuthLayout = () => {
-	const t = useTranslations("AuthPage");
-	return (
-		<Tabs defaultValue="signIn" className="w-[400px]">
-			<TabsList>
-				<TabsTrigger value="signIn">{t("signIn")}</TabsTrigger>
-				<TabsTrigger value="signUp">{t("signUp")}</TabsTrigger>
-			</TabsList>
-			<TabsContent value="signIn">
-				<SignInForm />
-			</TabsContent>
-			<TabsContent value="signUp">
-				<SignUpForm />
-			</TabsContent>
-		</Tabs>
-	);
+  const t = useTranslations("AuthPage");
+  return (
+    <Tabs defaultValue="signIn" className="w-[400px]">
+      <TabsList>
+        <TabsTrigger value="signIn">{t("signIn")}</TabsTrigger>
+        <div>1</div>
+        <TabsTrigger value="signUp">{t("signUp")}</TabsTrigger>
+      </TabsList>
+      <TabsContent value="signIn">
+        <SignInForm />
+      </TabsContent>
+      <TabsContent value="signUp">
+        <SignUpForm />
+      </TabsContent>
+    </Tabs>
+  );
 };
 
 export default AuthLayout;
