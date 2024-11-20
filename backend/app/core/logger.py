32fc022f9259faf0d7b4b1a20928c0270fa71d2f
Memetelve <memetelve1@gmail.com>
2024-11-20 15:31:05 +0100

    feat(backend): [NO-ISSUE] Add logging

--- backend/app/core/logger.py
+++ backend/app/core/logger.py
@@ -3,19 +3,17 @@ import logging.config
 import os
 
 from datetime import datetime
+from typing import Any
 
 
 class DynamicFileHandler(logging.FileHandler):
-	def __init__(self, base_filename, *args, **kwargs):
+	def __init__(self, base_filename: str, *args: Any, **kwargs: Any) -> None:
 		timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 		filename = f"{base_filename}_{timestamp}.log"
 		super().__init__(filename, *args, **kwargs)
 
 
-<<<<<<< HEAD
-=======
 # Create logs directory
->>>>>>> origin/feat/backend-logging
 os.makedirs("logs", exist_ok=True)
 
 # Modify the logging configuration
@@ -23,14 +21,10 @@ LOGGING_CONFIG = {
 	"version": 1,
 	"disable_existing_loggers": False,
 	"formatters": {
-<<<<<<< HEAD
 		"standard": {
 			"format": "%(asctime)s-%(name)-25s [%(levelname)-6s]: %(message)s",
 			"use_colors": True,
 		},
-=======
-		"standard": {"format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"},
->>>>>>> origin/feat/backend-logging
 	},
 	"handlers": {
 		"console": {"class": "logging.StreamHandler", "formatter": "standard", "level": "INFO"},
@@ -47,14 +41,10 @@ LOGGING_CONFIG = {
 			"level": "INFO",
 			"propagate": True,
 		},
-<<<<<<< HEAD
 		"uvicorn": {"handlers": ["file", "console"], "level": "INFO", "propagate": False},
-=======
-		"uvicorn": {"handlers": ["console", "file"], "level": "INFO", "propagate": False},
->>>>>>> origin/feat/backend-logging
 	},
 }
 
 
-def configure_logging():
+def configure_logging() -> None:
 	logging.config.dictConfig(LOGGING_CONFIG)
