175e3a451177fb1caddb5037368771a18cac2c3f
Mateusz Kurowski <m.kurowski.023@studms.ug.edu.pl>
2024-12-01 19:13:38 +0100

    feat(frontend): [PYTA-38] add intl to mode toggler

--- frontend/src/app/[locale]/_components/ModeToggle.tsx
+++ frontend/src/app/[locale]/_components/ModeToggle.tsx
@@ -11,28 +11,29 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useTranslations } from "next-intl";
 
 export function ModeToggle() {
 	const { setTheme } = useTheme();
-
+	const t = useTranslations("ModeToggler");
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
 				<Button variant="outline" size="icon">
 					<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 					<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-					<span className="sr-only">Toggle theme</span>
+					<span className="sr-only">t('changeMode')</span>
 				</Button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align="end">
 				<DropdownMenuItem onClick={() => setTheme("light")}>
-					Light
+					t('ligth')
 				</DropdownMenuItem>
 				<DropdownMenuItem onClick={() => setTheme("dark")}>
-					Dark
+					t('dark')
 				</DropdownMenuItem>
 				<DropdownMenuItem onClick={() => setTheme("system")}>
-					System
+					t('system')
 				</DropdownMenuItem>
 			</DropdownMenuContent>
 		</DropdownMenu>
