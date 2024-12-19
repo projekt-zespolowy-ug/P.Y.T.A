15c02730ab4a66df915fbc8f68297d34d0070a49
Mateusz Kurowski <m.kurowski.023@studms.ug.edu.pl>
2024-12-19 17:16:01 +0100

    refactor(frontend): [NO-ISSUE] auth layout and routing;

--- frontend/src/app/[locale]/auth/sign-in/page.tsx
+++ frontend/src/app/[locale]/auth/sign-in/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React from "react";
-import SignInForm from "./_components/SignInForm";
+
+import SignInForm from "../_components/SignInForm";
 
 const Page = () => {
 	return <SignInForm />;
