9085f72dc6f25597d02727e2b48aee8b6d1ef9ad
Memetelve <memetelve@gmail.com>
2024-12-19 09:05:16 +0100

    fix(backend): [PYTA-63] move stocks.py to correct dir

--- backend/app/v1/auth/__init__.py
+++ backend/app/v1/auth/__init__.py
@@ -1,4 +1,4 @@
 from app.v1.auth.auth_router import auth_router
-from app.v1.auth.stocks import stocks_router
+from app.v1.stocks.stocks import stocks_router
 
 __all__ = ["auth_router", "stocks_router"]
