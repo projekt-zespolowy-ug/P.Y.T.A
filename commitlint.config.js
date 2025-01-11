75ce797ebc154faf88545aba707d84cd64e31489
Memetelve <memetelve@gmail.com>
2025-01-11 03:11:33 +0100

    feat(backend): [PYTA-116] initialize the simulator with random, but consistent values

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,37 +1,39 @@
 module.exports = {
-	extends: ['@commitlint/config-conventional'],
+	extends: ["@commitlint/config-conventional"],
 	rules: {
-		'header-max-length': [2, 'always', 72],
-		'type-enum': [
+		"header-max-length": [2, "always", 100],
+		"type-enum": [
 			2,
-			'always',
+			"always",
 			[
-				'feat',
-				'fix',
-				'docs',
-				'style',
-				'refactor',
-				'perf',
-				'test',
-				'chore',
-				'build',
-				'ci',
-				'revert',
+				"feat",
+				"fix",
+				"docs",
+				"style",
+				"refactor",
+				"perf",
+				"test",
+				"chore",
+				"build",
+				"ci",
+				"revert",
 			],
 		],
-		'scope-enum': [2, 'always'],
-		'scope-empty': [2, 'never'],
-		'ticket-id': [2, 'always'],
+		"scope-enum": [2, "always"],
+		"scope-empty": [2, "never"],
+		"ticket-id": [2, "always"],
 	},
 	plugins: [
 		{
 			rules: {
-				'ticket-id': ({ header }) => {
+				"ticket-id": ({ header }) => {
 					const ticketIdPattern = /\[(PYTA-\d+|NO-ISSUE)\]/;
 					const isValid = ticketIdPattern.test(header);
 					return [
 						isValid,
-						isValid ? null : 'Commit message must contain a ticket ID in the format [PYTA-1]',
+						isValid
+							? null
+							: "Commit message must contain a ticket ID in the format [PYTA-1]",
 					];
 				},
 			},
