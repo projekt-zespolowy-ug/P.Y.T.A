f0e765e595817b44656472394a51ea2544a9087c
Mateusz Kurowski <134972002+mateusz-kurowskiUG@users.noreply.github.com>
2025-05-27 20:52:12 +0200

    fix(frontend): [NO-ISSUE] fix build (#179)

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+	  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'output-[hash].xml',
+  },
 	e2e: {
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
